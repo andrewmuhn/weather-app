8db1b8389b749484b98c6b8516615947135dc82f
andrew <andrewmuhn@gmail.com>
2023-02-28 16:14:47 -0800

    setup up psuedo code and basic function outline

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+const apiKey = '64a57b8b595143bde642b3a647236223';
+
+//!psuedo code
+//take input city from user entry and convert it into the applicable parameters to append to the request urls for both current and 5 day forecast
+// - save city and attached urls to local storage
+// - append button of city to search list so user can click on previously searched cities.
+//extract needed information (temp, wind, humidity) from returned JSON object.
+// - append above info into html document
+//
+
+
+//TODO: creath fetch request that accepts a URL varaible
+fetch(requestUrl)
+    .then((response) => {
+
+        return response.json();
+    })
+    .then((data) => {
+        console.log('Fetch Response \n----------------');
+        console.log(data);
+    })
+
+//should take user inputs and extract applicable parmetrs and fire off functions to fetch geocode from city name.
+const handleFormSubmit = () => {
+
+}
+
+//fires fetchh request for geocoded info. fires off functions to update and save local storage. fires off fetch for current weather and 5-day forecast
+const searchGeocode = () => {
+
+}
+
+//takes geocode info and fires of fetch request for current weather and 5-day weather.  fires off printWeatherData
+const searchCityWeather = () => {
+
+}
+
+//extracts values from retured JSON weather data and displays them in html file
+const printWeatherData = () => {
+
+}
+
+//save an an array of objects to local storage with user input as key and fetched geocode as value. fires off readromLocalStorage
+const saveToLocalStorage = () => {
+
+}
+
+//parses data from local storage. if no dat hen reurn an empty array.firest off printCityList
+const readFromLocalStorage = () => {
+
+}
+
+//take parsed data from local storage and save it in the form of a button that is appended to the search list
+const printCityList = () => {
+
+}
+
+//TODO: create event handlers for:
+// -search button to fire off handleFormSubmit
+// -appended city buttons to fire off fetchCityWeather
